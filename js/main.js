e750e9820231080dd1f2fcc949daf1fc50c88d94
Adamy123 <yap61037@gmail.com>
2024-05-26 16:34:42 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          target.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
